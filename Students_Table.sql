2de8b7c26748cf24acad8fb2fcaaf2b2fedf323d
samirashoi <issamrasheed@gmail.com>
2018-10-18 13:31:31 +0530

    Add Sample Data to StudentMS

---
+++ Students_Table.sql
@@ -0,0 +1,60 @@
+create table Students_Table (
+	id INT,
+	first_name VARCHAR(50),
+	last_name VARCHAR(50),
+	dob DATE,
+	gender VARCHAR(50),
+	CGPA DECIMAL(4,2),
+	address VARCHAR(50),
+	email VARCHAR(50)
+);
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (1, 'Burt', 'Adshed', '09-Sep-2000', 'Male', 4.79, 'Lubomierz', 'badshed0@about.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (2, 'Raymund', 'Hatherleigh', '15-Jul-1999', 'Male', 5.76, 'Longbei', 'rhatherleigh1@xrea.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (3, 'Cindelyn', 'McGurn', '12-Feb-2000', 'Female', 5.71, 'Shu', 'cmcgurn2@ycombinator.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (4, 'Freddy', 'Dyche', '19-Sep-1999', 'Male', 2.37, 'Beringinjaya', 'fdyche3@wikimedia.org');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (5, 'Lon', 'Guilloud', '27-Dec-1999', 'Male', 2.24, 'Guilmaro', 'lguilloud4@people.com.cn');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (6, 'Veronica', 'Dawdary', '05-Apr-1999', 'Female', 8.9, 'Foz Giraldo', 'vdawdary5@newyorker.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (7, 'Patrizius', 'Farmloe', '14-Jun-1997', 'Male', 3.25, 'Villefranche-sur-Mer', 'pfarmloe6@blogger.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (8, 'Ambrosio', 'Shayler', '04-Sep-2000', 'Male', 5.03, 'Węgierska Górka', 'ashayler7@w3.org');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (9, 'Myca', 'Daugherty', '14-Jun-1998', 'Male', 2.78, 'Kuršumlija', 'mdaugherty8@smh.com.au');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (10, 'Rebe', 'Aldus', '25-Oct-1996', 'Female', 9.77, 'Pacaembu', 'raldus9@delicious.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (11, 'Joela', 'Broz', '25-Jan-1997', 'Female', 5.35, 'Debark’', 'jbroza@google.com.au');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (12, 'Dana', 'McCluney', '03-Apr-2000', 'Male', 1.17, 'Tianya', 'dmccluneyb@wufoo.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (13, 'Alejandrina', 'Paver', '29-Oct-1997', 'Female', 8.85, 'Santai', 'apaverc@youtube.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (14, 'Agnella', 'Halford', '15-Jan-1997', 'Female', 3.94, 'Ouésso', 'ahalfordd@surveymonkey.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (15, 'Clerissa', 'Simak', '22-Jan-1997', 'Female', 7.32, 'Det Udom', 'csimake@nifty.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (16, 'Ermengarde', 'Wixon', '09-Nov-1999', 'Female', 2.74, 'Tečovice', 'ewixonf@usa.gov');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (17, 'Moore', 'Basnett', '03-Mar-1997', 'Male', 2.42, 'Orosh', 'mbasnettg@nba.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (18, 'Rabbi', 'Pickervance', '22-Jul-1999', 'Male', 9.25, 'Aubagne', 'rpickervanceh@rakuten.co.jp');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (19, 'Tisha', 'Chesshire', '05-Nov-1997', 'Female', 5.65, 'San Cristóbal Totonicapán', 'tchesshirei@free.fr');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (20, 'Samson', 'Dumbleton', '30-Dec-1996', 'Male', 4.04, 'Villa Hayes', 'sdumbletonj@hp.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (21, 'Giordano', 'Barnewall', '26-May-2000', 'Male', 1.52, 'Limit', 'gbarnewallk@army.mil');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (22, 'Liane', 'Mulgrew', '24-Dec-1999', 'Female', 3.5, 'Bailianhe', 'lmulgrewl@pbs.org');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (23, 'Bryanty', 'Jenney', '01-Dec-1999', 'Male', 9.35, 'Sąspów', 'bjenneym@ca.gov');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (24, 'Allison', 'Newbigging', '12-Jan-1997', 'Female', 1.19, 'A Yun Pa', 'anewbiggingn@bbc.co.uk');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (25, 'Iago', 'Summerill', '07-Mar-1998', 'Male', 3.21, 'Liopétri', 'isummerillo@1688.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (26, 'Michal', 'Cherm', '10-Feb-2000', 'Male', 5.83, 'Kamimaruko', 'mchermp@hexun.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (27, 'Cristiano', 'Gartell', '08-Nov-1997', 'Male', 9.35, 'Polo', 'cgartellq@google.nl');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (28, 'Constance', 'Holleran', '12-Nov-1998', 'Female', 6.01, 'Gif-sur-Yvette', 'cholleranr@usa.gov');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (29, 'Stoddard', 'Webburn', '10-Sep-2000', 'Female', 7.36, 'Sadek', 'swebburns@zimbio.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (30, 'Merci', 'Savidge', '19-Aug-1997', 'Female', 6.11, 'Tanggung', 'msavidget@weather.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (31, 'Lauraine', 'Sangster', '15-Feb-1998', 'Female', 3.18, 'Esmeraldas', 'lsangsteru@mlb.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (32, 'Elnar', 'Piercey', '20-Nov-1999', 'Male', 1.55, 'Pensacola', 'epierceyv@wordpress.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (33, 'Orson', 'Orwell', '30-Mar-1997', 'Male', 4.29, 'Wangyi Zhendian', 'oorwellw@so-net.ne.jp');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (34, 'Adelle', 'Busswell', '16-Jul-2000', 'Female', 6.22, 'Tsibulev', 'abusswellx@usgs.gov');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (35, 'Joseito', 'Dacey', '24-Feb-1997', 'Male', 2.9, 'Yirga ‘Alem', 'jdaceyy@msn.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (36, 'Sabina', 'McCallister', '03-Jun-1998', 'Female', 4.18, 'Ilovka', 'smccallisterz@miitbeian.gov.cn');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (37, 'Idelle', 'Lapworth', '10-Aug-2000', 'Female', 9.32, 'Buenos Aires', 'ilapworth10@engadget.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (38, 'Gustave', 'Fouracres', '26-Feb-1997', 'Male', 5.92, 'Shawnee Mission', 'gfouracres11@51.la');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (39, 'Celine', 'O''Fihily', '03-Sep-1999', 'Female', 5.52, 'Turija', 'cofihily12@shop-pro.jp');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (40, 'Tammi', 'Goldes', '16-May-2000', 'Female', 2.26, 'Yezhi', 'tgoldes13@wikimedia.org');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (41, 'Rudolf', 'Ward', '29-Apr-1997', 'Male', 6.69, 'Shatian', 'rward14@live.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (42, 'Base', 'Dangerfield', '20-Feb-2000', 'Male', 1.3, 'Penza', 'bdangerfield15@cocolog-nifty.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (43, 'Olga', 'Simeoni', '04-Sep-2000', 'Female', 3.27, 'Kiihtelysvaara', 'osimeoni16@sciencedirect.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (44, 'Pepe', 'Throssell', '13-May-1998', 'Male', 6.11, 'Kosti', 'pthrossell17@liveinternet.ru');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (45, 'Barnie', 'Hallibone', '08-Apr-1998', 'Male', 6.65, 'Mölndal', 'bhallibone18@joomla.org');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (46, 'Sterne', 'Boldra', '09-Feb-1999', 'Male', 8.26, 'Shuigou’ao', 'sboldra19@bloglines.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (47, 'Ephrayim', 'Reedshaw', '28-Apr-2000', 'Male', 6.39, 'Ágios Efstrátios', 'ereedshaw1a@google.it');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (48, 'Aleta', 'Horrod', '08-Dec-1999', 'Female', 1.19, 'Kuching', 'ahorrod1b@craigslist.org');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (49, 'Leon', 'Dolohunty', '13-Aug-2000', 'Male', 8.88, 'Issad', 'ldolohunty1c@cargocollective.com');
+insert into Students_Table (id, first_name, last_name, dob, gender, CGPA, address, email) values (50, 'Bertrando', 'Kitchenside', '28-Oct-1998', 'Male', 5.5, 'Jingdezhen', 'bkitchenside1d@slashdot.org');
